89e50bd4195ad839e6a35bbf0d949c0da65217b0
Timothy Georgeson <timothy.georgeson@mobileappscompany.us>
2018-12-25 15:14:42 -0500

    On step 12/17

--- app/src/main/java/com/example/consultants/roomwordsample/ui/WordActivity.kt
+++ app/src/main/java/com/example/consultants/roomwordsample/ui/WordActivity.kt
@@ -3,13 +3,15 @@ package com.example.consultants.roomwordsample.ui
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import com.example.consultants.roomwordsample.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class WordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/consultants/roomwordsample/ui/WordViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.consultants.roomwordsample.ui
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import com.example.consultants.roomwordsample.data.Word
+import com.example.consultants.roomwordsample.data.WordRepository
+import com.example.consultants.roomwordsample.data.WordRoomDatabase
+import kotlinx.coroutines.*
+import kotlinx.coroutines.android.Main
+import kotlin.coroutines.CoroutineContext
+
+class WordViewModel(application: Application): AndroidViewModel(application) {
+
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = scope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
